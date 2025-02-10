3cdd555e5fa85ca573e6188c193eaa026759a7a8
Wes Dunphy <wesdunphy@mac.myfiosgateway.com>
2025-02-10 13:25:15 -0500

    add gitignore

---
+++ week_3_homework/ingest_taxi.py
@@ -0,0 +1,17 @@
+import io
+import os
+import requests
+import pandas as pd
+from google.cloud import storage
+
+"""
+Pre-reqs: 
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
+"""
+
+# services = ['fhv','green','yellow']
+init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
+# switch out the bucketname
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
