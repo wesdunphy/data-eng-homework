dc48639f0123976013d9793d0f80cd70aa89c9a6
Wes Dunphy <wesdunphy@mac.myfiosgateway.com>
2025-02-11 15:00:45 -0500

    basic stuff in here

--- week_3_homework/ingest_taxi.py
+++ week_3_homework/ingest_taxi.py
@@ -1,5 +1,7 @@
-import dlt
 import os
+import requests
+import pandas as pd
+from google.cloud import storage
 
 """
 Pre-reqs: 
@@ -9,7 +11,51 @@ Pre-reqs:
 """
 
 # services = ['fhv','green','yellow']
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
-
+init_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
 # switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "data-zoomcamp-wesdunphy-manual-bucket")
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+def web_to_gcs(year, service):
+    for i in range(12):
+        
+        # sets the month part of the file_name string
+        month = '0'+str(i+1)
+        month = month[-2:]
+
+        # csv file_name
+        file_name = f"{service}_tripdata_{year}-{month}.parquet"
+
+        # download it using requests via a pandas df
+        request_url = f"{init_url}{file_name}"
+        r = requests.get(request_url)
+        open(file_name, 'wb').write(r.content)
+        print(f"Local: {file_name}")
+
+        # read it back into a parquet file
+        df = pd.read_parquet(file_name)
+        file_name = file_name.replace('.csv.gz', '.parquet')
+        df.to_parquet(file_name, engine='pyarrow')
+        print(f"Parquet: {file_name}")
+
+        # upload it to gcs 
+        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+        print(f"GCS: {service}/{file_name}")
+
+
+web_to_gcs('2022', 'green')
